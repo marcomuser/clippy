f7913a2eddda2147aaa4e6e474d493652b689e02
Marco Muser <marco.muser@posteo.de>
2023-06-10 12:38:48 +0200

    refactor: extract CopyCard

---
+++ src/components/CopyCards/CopyCard.tsx
@@ -0,0 +1,46 @@
+import { Button, Card, Form, FormInstance, Typography } from "antd";
+import { type ReactNode, useRef } from "react";
+import { FormFields } from "../UserInputs/UserInputs";
+
+const { Text } = Typography;
+
+type TProps = {
+  title: string;
+  form: FormInstance<FormFields>;
+  children: ReactNode;
+};
+
+export const CopyCard = ({ title, form, children }: TProps) => {
+  const textRef = useRef<HTMLSpanElement>(null);
+
+  const firstName = Form.useWatch("firstName", form);
+  const surname = Form.useWatch("surname", form);
+
+  const onClickHandler = () => {
+    navigator.clipboard.writeText(
+      textRef.current
+        ? textRef.current.innerText
+        : "Failed to copy text. Please try again"
+    );
+
+    if (firstName && surname) {
+      const storage = window.localStorage;
+      storage.setItem("firstName", firstName);
+      storage.setItem("surname", surname);
+    }
+  };
+
+  return (
+    <Card
+      style={{ width: 400 }}
+      title={title}
+      actions={[
+        <Button type="primary" onClick={onClickHandler}>
+          Copy
+        </Button>,
+      ]}
+    >
+      <Text ref={textRef}>{children}</Text>
+    </Card>
+  );
+};
--- src/components/CopyCards/CopyCards.tsx
+++ src/components/CopyCards/CopyCards.tsx
@@ -1,56 +1,31 @@
-import { Button, Card, Form, type FormInstance, Typography } from "antd";
+import { Form, type FormInstance } from "antd";
 import { type FormFields } from "../UserInputs/UserInputs";
-import { useRef } from "react";
-
-const { Text } = Typography;
+import { CopyCard } from "./CopyCard";
+import styles from "./CopyCards.module.css";
 
 type TProps = {
   form: FormInstance<FormFields>;
 };
 
 export const CopyCards = ({ form }: TProps) => {
-  const textRef = useRef<HTMLSpanElement>(null);
-
-  const firstName = Form.useWatch("firstName", form);
-  const surname = Form.useWatch("surname", form);
+  const firstName = Form.useWatch("firstName", form) ?? "Vorname";
+  const surname = Form.useWatch("surname", form) ?? "Nachname";
   const dateTime = new Intl.DateTimeFormat("de-DE", {
     dateStyle: "medium",
     timeStyle: "short",
   }).format(new Date());
 
-  const onClickHandler = () => {
-    navigator.clipboard.writeText(
-      textRef.current
-        ? textRef.current.innerText
-        : "Failed to copy text. Please try again"
-    );
-
-    if (firstName && surname) {
-      const storage = window.localStorage;
-      storage.setItem("firstName", firstName);
-      storage.setItem("surname", surname);
-    }
-  };
-
   return (
-    <Card
-      style={{ width: 400 }}
-      title="Kunde noch nicht erreicht"
-      actions={[
-        <Button type="primary" onClick={onClickHandler}>
-          Copy
-        </Button>,
-      ]}
-    >
-      <Text ref={textRef}>
+    <section className={styles.cards}>
+      <CopyCard form={form} title="Kunde noch nicht erreicht">
         RETENTION 📞❌ STORNO NOCH NICHT ERREICHT
-        <br />❌ NE1 {firstName ?? "Vorname"} {surname ?? "Nachname"} {dateTime}
+        <br />❌ NE1 {firstName} {surname} {dateTime}
         <br />
         ❌ NE2
         <br />❌ NE3
         <br />
         ////////////////////////////////////////////////////////////////
-      </Text>
-    </Card>
+      </CopyCard>
+    </section>
   );
 };
