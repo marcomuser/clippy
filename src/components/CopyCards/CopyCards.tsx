139647105e3af7035a7f9c447597701231a54277
LukaAraz <142007456+LukaAraz@users.noreply.github.com>
2023-12-16 13:31:58 +0100

    Update CopyCards.tsx

--- src/components/CopyCards/CopyCards.tsx
+++ src/components/CopyCards/CopyCards.tsx
@@ -28,17 +28,15 @@ export const CopyCards = ({ form }: TProps) => {
   return (
     <section className={styles.cards}>
       <CopyCard form={form} title="Nicht erreicht">
-        <br />❌ NE {firstName} {surname} {dateTime}
+        <br /> NE {firstName} {surname} {dateTime} ❌
       </CopyCard>
 
       <CopyCard form={form} title="Kunde noch nicht erreicht">
         RETENTION 📞❌ STORNO NOCH NICHT ERREICHT
         <br />❌ NE1 {firstName} {surname} {dateTime}
-        <br />
-        ❌ NE2
+        <br />❌ NE2
         <br />❌ NE3
-        <br />
-        ❌ NE4
+        <br />❌ NE4
         <br />❌ NE5
         <br />❌ NE6
         <br />❌ NE7
@@ -52,6 +50,8 @@ export const CopyCards = ({ form }: TProps) => {
 <CopyCard form={form} title="PRIO 1 Bucket">
         🔴 RETENTION PRIO 1 Bucket 🔴 <br />
         {firstName} {surname} {dateTime} DAUER
+        <br /> Mosaik: Ja/Nein 
+        <br /> Musterprojekt: 1,5....
         <br />
         <br /> ✅ FU Datum/Uhrzeit:
         <br />
@@ -61,6 +61,12 @@ export const CopyCards = ({ form }: TProps) => {
         🅰️ Stornogründe Stichpunkte: <br />
         <br />
         🅱️ Notizen aus Call: <br />
+         <br />
+        <br /> Warum wollte der Kunde eine PV Anlage? (Bedarfsanalyse)
+        <br />
+        <br /> Was fand der Kunde interessant an Enpal?
+        <br />
+        <br />
         <br />
         ////////////////////////////////////////////////////////////////
       </CopyCard>
@@ -68,6 +74,8 @@ export const CopyCards = ({ form }: TProps) => {
       <CopyCard form={form} title="PRIO 1.5 Bucket">
         🔴 RETENTION PRIO 1.5 Bucket 🔴 <br />
         {firstName} {surname} {dateTime} DAUER
+        <br /> Mosaik: Ja/Nein 
+        <br /> Musterprojekt: 1,5....
         <br />
         <br /> ✅ FU Datum/Uhrzeit:
         <br />
@@ -77,6 +85,12 @@ export const CopyCards = ({ form }: TProps) => {
         🅰️ Stornogründe Stichpunkte: <br />
         <br />
         🅱️ Notizen aus Call: <br />
+         <br />
+        <br /> Warum wollte der Kunde eine PV Anlage? (Bedarfsanalyse)
+        <br />
+        <br /> Was fand der Kunde interessant an Enpal?
+        <br />
+        <br />
         <br />
         ////////////////////////////////////////////////////////////////
       </CopyCard>
@@ -84,6 +98,8 @@ export const CopyCards = ({ form }: TProps) => {
       <CopyCard form={form} title="PRIO 2 Bucket">
         🔴 RETENTION PRIO 2 Bucket 🔴 <br />
         {firstName} {surname} {dateTime} DAUER
+        <br /> Mosaik: Ja/Nein 
+        <br /> Musterprojekt: 1,5....
         <br />
         <br /> ✅ FU Datum/Uhrzeit:
         <br />
@@ -93,6 +109,12 @@ export const CopyCards = ({ form }: TProps) => {
         🅰️ Stornogründe Stichpunkte: <br />
         <br />
         🅱️ Notizen aus Call: <br />
+         <br />
+        <br /> Warum wollte der Kunde eine PV Anlage? (Bedarfsanalyse)
+        <br />
+        <br /> Was fand der Kunde interessant an Enpal?
+        <br />
+        <br />
         <br />
         ////////////////////////////////////////////////////////////////
       </CopyCard>
@@ -100,6 +122,8 @@ export const CopyCards = ({ form }: TProps) => {
       <CopyCard form={form} title="PRIO 3 Bucket">
         🔴 RETENTION PRIO 3 Bucket 🔴 <br />
         {firstName} {surname} {dateTime} DAUER
+        <br /> Mosaik: Ja/Nein 
+        <br /> Musterprojekt: 1,5....
         <br />
         <br /> ✅ FU Datum/Uhrzeit:
         <br />
@@ -110,10 +134,16 @@ export const CopyCards = ({ form }: TProps) => {
         <br />
         🅱️ Notizen aus Call: <br />
         <br />
+        <br /> Warum wollte der Kunde eine PV Anlage? (Bedarfsanalyse)
+        <br />
+        <br /> Was fand der Kunde interessant an Enpal?
+        <br />
+        <br />
+        <br />
         ////////////////////////////////////////////////////////////////
       </CopyCard>
 
-      <CopyCard form={form} title="SOS Fälle">
+      <CopyCard form={form} title="SOS IDN">
         🆘 SOS TBK (live) Retention Manager: {firstName} {surname} - {dateTime}{" "}
         / Dauer
         <br /> // 📥 Case via (von wem kam die KTL?):
@@ -136,6 +166,29 @@ export const CopyCards = ({ form }: TProps) => {
         ////////////////////////////////////////////////////////////////
       </CopyCard>
 
+      <CopyCard form={form} title="SOS Live Home Improvement">
+        🆘 SOS HOME (live) Retention Manager: {firstName} {surname} - {dateTime}{" "}
+        / Dauer
+        <br /> // 📥 Case via (von wem kam die KTL?):
+        <br />
+        <br /> // ⚠ Kurzbeschreibung aus SOS Chat:
+        <br />
+        <br /> // ☀️ Link zur CL/KTL:
+        <br />
+        <br />
+        // ✅ FU Datum/Uhrzeit:
+        <br />
+        <br /> // 💚 CL/KTL geschlossen?:
+        <br />
+        <br /> 🅰️ Stornogründe Stichpunkte:
+        <br />
+        <br />
+        RETENTION 🅱️ Notizen aus Call:
+        <br />
+        <br />
+        ////////////////////////////////////////////////////////////////
+      </CopyCard>
+      
       <CopyCard form={form} title="After IPA / SC2 case ">
         ☀️Kunde im Retention Prozess ☀️
         <br />
@@ -144,6 +197,16 @@ export const CopyCards = ({ form }: TProps) => {
         und diesen SC2, samt Terminierung. Wärst du so freundlich und gehst zum
         Wohle des Kunden bei Rückfragen bitte zuerst auf deine/n TL zu, bevor du
         den Kunden kontaktierst? Dein/e TL ist herzlich eingeladen sich beim Retention Lead zu melden, falls es Fragen gibt.
+        <br />
+        Vielen Dank und liebe Grüße {firstName} {surname}, {dateTime}
+      </CopyCard>
+      
+       <CopyCard form={form} title="Sonderprojekt Mosaik">
+        Ⓜ️MOSAIK SONDERPROJEKTⓂ️
+        <br />
+        Hallo liebes Sales, dieser Kunde liegt im Mosaik Sonderprojekt. 
+        <br /> Bitte nicht mehr kontaktieren. 
+        <br /> Vielen Dank ☀️
         <br />
         Vielen Dank und liebe Grüße {firstName} {surname}, {dateTime}
       </CopyCard>
